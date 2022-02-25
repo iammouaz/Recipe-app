e2ba09527d691cdc72f8b5c6c65a41a88d17b325
iammouaz <88675267+iammouaz@users.noreply.github.com>
2022-02-25 17:19:56 +0200

    Add new controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def after_sign_in_path_for(_resource)
+    foods_path
+  end
+
+  def after_sign_out_path_for(_resource)
+    new_user_session_path
+  end
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :email, :password, :current_password)
+    end
+  end
 end
